50eafd4d4df17c3141382a919d6923002e731c6d
MrFlash67 <crispinh04@gmail.com>
2013-07-01 10:06:49 +1200

    Added no newline at final print statement at the end of intro.

--- bombstuff.py
+++ bombstuff.py
@@ -3,6 +3,6 @@ import sys, curses, time, myGetCh #to be added to
 print 'Welcome to ...'
 time.sleep(3)
 print 'A game about exploding bananas with guns!' #WIP line there
-print 'Press enter to continue.'
+print 'Press enter to continue.',
 raw_input()
 ''' End intro. WIP. '''
