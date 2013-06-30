cef4512d337652e482140706f9d9912b81588ba2
MrFlash67 <c.hitchingsanstice@comp-student-lion.discovery1.school.nz>
2013-07-01 10:01:46 +1200

    Added intro...
    And possibly unneeded module.

---
+++ bombstuff.py
@@ -0,0 +1,8 @@
+import sys, curses, time, myGetCh #to be added to
+''' Start intro. WIP. '''
+print 'Welcome to ...'
+time.sleep(3)
+print 'A game about exploding bananas with guns!' #WIP line there
+print 'Press enter to continue.'
+raw_input()
+''' End intro. WIP. '''
